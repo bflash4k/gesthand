9a13ddd690fa5d12e763aed3e93b77642e08961c
Olivier Guiter <oguiter@free.fr>
2017-10-08 18:58:05 +0200

    Remove uneeded files

--- index.php
+++
@@ -1,8 +0,0 @@
-
-	<hr>Gesthand Events Calendar<hr>
-    <form action="login.php" method="post">
-    	 <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-        Gesthand Calendar CSV file: <input name="csv_file" type="file">
-        <input type="submit">
-    </form>
-
